79c0b6e568f09cfec921dc2e115bb59e10632db9
Jin Soo Lim <jinsoolim1@gmail.com>
2020-10-07 17:43:35 -0700

    asdf

---
+++ server/routers/login.js
@@ -0,0 +1,23 @@
+const { Router } = require('express');
+const express = require('express');
+const userController = require('../controllers/UserController');
+const UserController = require('../controllers/UserController');
+const router = express.Router();
+
+router.post('/login', 
+  userController.createUser, 
+  (req, res) => res.status(200).json(res.locals.data)
+);
+
+router.post('/getUserData', 
+  userController.sendUserData, 
+  (req, res) => res.status(200).json(res.locals.data)
+);
+
+router.post('/deleteUser/:id', 
+  userController.deleteUserData, 
+  (req, res) => res.status(200).json(res.locals.data)
+);
+
+module.exports = router;
+
